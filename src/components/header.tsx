d0832c88194bb7905e823ab8a082945785e2fa76
virginia <virginiamaiabf@gmail.com>
2024-02-20 21:41:44 -0300

    fix: correction name

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -68,11 +68,11 @@ export function Header() {
 
       <div className="flex items-center gap-3">
         <div className="flex flex-col items-end gap-0.5">
-          <span className="text-sm font-medium">Diego Fernandes</span>
-          <span className="text-xs text-zinc-400">diego@nivo.video</span>
+          <span className="text-sm font-medium">Virgínia Maia</span>
+          <span className="text-xs text-zinc-400">virginia@nivo.video</span>
         </div>
         <img
-          src="https://github.com/diego3g.png"
+          src="https://avatars.githubusercontent.com/u/19542923?v=4"
           className="size-8 rounded-full"
           alt=""
         />
